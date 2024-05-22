7cac93f6e5bfc785e9f374c97b8b5dd6b42efe21
Chunwei Xia <120237776@qq.com>
2024-05-23 06:18:29 +0800

    Add function to evaluate whether an assembly is wright

--- examples/train_io.py
+++ examples/train_io.py
@@ -3,7 +3,14 @@ from typing import Optional, Tuple, Dict
 
 import datasets
 from datasets import load_dataset, load_from_disk
-from exebench import Wrapper, diff_io, exebench_dict_to_dict, LLVMAssembler, cpp2ass
+from exebench import (Wrapper, LLVMAssembler, diff_io, exebench_dict_to_dict,
+                      cpp2ass, preprocessing_c_deps, eval_assembly)
+import logging
+
+logging.basicConfig(
+    format=
+    "%(asctime)s - %(levelname)s - %(filename)s - Line: %(lineno)d - %(message)s",
+)
 
 
 def dump_sample_for_splits(path_to_dataset):
@@ -23,37 +30,25 @@ def dump_sample_for_splits(path_to_dataset):
             break
 
 
-def preprocessing_c_deps(row) -> str:
-    c_deps = ""
-    c_deps += row['real_deps'] +"\n" if row['real_deps'] is not None else ""
-    replacements = [
-        "# 1",
-    ]
-    for r in replacements:
-        if c_deps.find(r) != -1:
-            c_deps = c_deps.replace(r, "").strip()
-    
-    if c_deps == "" and row['synth_deps'] is not None:
-        c_deps += row['synth_deps'] +"\n"
-    return c_deps + "\n"
-
-
 def compile_assembly(row) -> Dict:
     c_deps = preprocessing_c_deps(row)
     try:
-        success, _, ass = cpp2ass(c_deps + row["func_def"])
+        success, llvm_ir, ass = cpp2ass(c_deps + row["func_def"])
         if success:
-            return c_deps, ass
-    except:
-        print(f"Error for {row['path']}")
+            return c_deps, llvm_ir, ass
+    except Exception as e:
+        logging.error(f"Error for {row['path']} with {e}")
 
-    return c_deps, None
+    return c_deps, None, None
+
 
 # if success:
 #     row["asm"]['target'].append('real_llvm_X86_O2')
 #     row["asm"]['code'].append(ass)
 
-def test_compile_and_run_exebench_sample(path_to_dataset: str, num_samples: int = 1):
+
+def test_compile_and_run_exebench_sample(path_to_dataset: str,
+                                         num_samples: int = 1):
     # 1) Load dataset split. In this case, synthetic test split
     dataset = load_dataset(
         path_to_dataset,
@@ -62,14 +57,12 @@ def test_compile_and_run_exebench_sample(path_to_dataset: str, num_samples: int 
     print("Load dataset")
     success, fail = 0, 0
     for row in dataset.select(range(0, num_samples)):
-        c_deps, ass = compile_assembly(row)
-        print("*"*20)
+        c_deps, llvm_ir, ass = compile_assembly(row)
+        print("*" * 20)
         print(c_deps)
-
         print(ass)
-
         print(row['real_exe_wrapper'])
-        print("*"*20)
+        print("*" * 20)
         if ass is None:
             fail += 1
             continue
@@ -80,12 +73,12 @@ def test_compile_and_run_exebench_sample(path_to_dataset: str, num_samples: int 
             cpp_wrapper=row['real_exe_wrapper'],
             assembler_backend=LLVMAssembler(),
             # func_def=row["func_def"]
-            )
+        )
         observed_output = synth_wrapper(
             exebench_dict_to_dict(row['real_io_pairs']['input']
                                   [0]))  # Run synthetic example number 0
         if observed_output is None:
-            print('Error: The code could not be compiled')
+            logging.error('Error: The code could not be compiled')
         else:
             print('Input',
                   exebench_dict_to_dict(row['real_io_pairs']['input'][0]))
@@ -97,63 +90,97 @@ def test_compile_and_run_exebench_sample(path_to_dataset: str, num_samples: int 
                                row['real_io_pairs']['output'][0])) else 'No')
 
 
-def main2():
+def test_default_compile_and_run_exebench_sample(path_to_dataset: str,
+                                                 num_samples: int = 1):
     # 1) Load dataset split. In this case, synthetic test split
     dataset = load_dataset(
-        "/data/xiachunwei/Datasets/exebench",
+        path_to_dataset,
         split='train_real_simple_io')  # , use_auth_token=True)
     # 2) Iterate over dataset
     print("Load dataset")
-    count = 0
-    for row in dataset:
-        for key, value in row.items():
-            print(f"key: {key}, value: {value}")
+    success, fail = 0, 0
+    for row in dataset.select(range(0, num_samples)):
+        c_deps, ass = compile_assembly(row)
         print("*" * 20)
-        count += 1
-        c_deps = ""
-        c_deps += row['real_deps'] + "\n" if row[
-            'real_deps'] is not None else ""
-        # c_deps += row['synth_deps'] +"\n" if row['synth_deps'] is not None else ""
-        # c_deps += row['synth_io_pairs']['dummy_funcs'][0] if row['synth_io_pairs'] is not None else ""
-        # c_deps += f'\nextern {row["func_head_types"].replace("extern", "")};\n'
-        # c_deps = preprocessing_c_deps(c_deps)
-        c_deps += "#define bool char\n"
-        print("c_deps:", c_deps)
-        print("func_def:", row["func_def"])
-        ass, success = cpp2ass(c_deps + row["func_def"])
+        print(c_deps)
+        print(ass)
+        print(row['real_exe_wrapper'])
         print("*" * 20)
-        if count >= 1:
-            break
-    print(count)
+        if ass is None:
+            fail += 1
+            continue
+        try:
+            synth_wrapper = Wrapper(
+                c_deps=c_deps + '\n',
+                func_c_signature=row['func_head_types'].replace('extern', ''),
+                func_assembly=row["asm"]['code'][0],
+                cpp_wrapper=row['real_exe_wrapper'],
+                # assembler_backend=LLVMAssembler(),
+                # func_def=row["func_def"]
+            )
+            observed_output = synth_wrapper(
+                exebench_dict_to_dict(row['real_io_pairs']['input']
+                                      [0]))  # Run synthetic example number 0
+            if observed_output is None:
+                logging.error('Error: The code could not be compiled')
+            else:
+                print('Input',
+                      exebench_dict_to_dict(row['real_io_pairs']['input'][0]))
+                print('Observed Output:', observed_output)
+                print(
+                    'Does this output coincide with the expected one?',
+                    'Yes' if diff_io(
+                        observed_output=observed_output,
+                        expected_output=exebench_dict_to_dict(
+                            row['real_io_pairs']['output'][0])) else 'No')
+        except Exception as e:
+            logging.error(f"Error for {row['path']}")
+            logging.error(e)
+
+
+def compile_row_with_assembly(row: Dict) -> Dict:
+    c_deps = preprocessing_c_deps(row)
+    try:
+        success, llvm_ir, ass = cpp2ass(c_deps + row["func_def"])
+        if success:
+            row["asm"]["code"].append(ass)
+            row["asm"]["target"].append("real_clang15_x86_O2")
+            row["llvm_ir"] = {
+                "code": [
+                    llvm_ir,
+                ],
+                "target": [
+                    "real_clang15_x86_O2",
+                ]
+            }
+    except Exception as e:
+        logging.error(f"Error for {row['path']} with {e}")
+    row["llvm_ir"] = {"code": [], "target": []}
+    return row
 
 
 def get_all_assembly(path_to_dataset,
+                     path_to_saved_dataset,
                      split_name: str = "train_real_simple_io"):
     dataset = load_dataset(path_to_dataset,
                            split=split_name)  # , use_auth_token=True)
     # for row in dataset.select((0, 100)):
     #     print(row)
-    dataset_with_assembly = dataset.map(compile_assembly, num_proc=40)
+    dataset_with_assembly = dataset.map(compile_row_with_assembly, num_proc=40)
     # dataset_with_assembly = dataset.select((0, 100)).map(compile_assembly, batched=True, batch_size=20)
-    dataset_with_assembly.save_to_disk(
-        "/data/xiachunwei/Datasets/exebench/train_real_simple_io-llvm-assembly-batch"
-    )
-
-    for row in dataset_with_assembly:
-        print(row["asm"]['code'][-1])
-        break
+    dataset_with_assembly.save_to_disk(path_to_saved_dataset)
 
 
 def convert_to_instruction_format(path_to_dataset, path_to_json):
     dataset = load_from_disk(path_to_dataset)
     programs_list = []
     for row in dataset:
-        if row["asm"]['target'][-1] == "real_llvm_X86_O2":
+        if row["asm"]['target'][-1] == "real_clang15_x86_O2":
             programs_list.append({
                 "file":
                 row['path'],
                 "output":
-                row["asm"]['code'][-1],
+                row["llvm_ir"]["code"][-1],
                 "input":
                 row["asm"]['code'][-1],
                 "instruction":
@@ -162,9 +189,37 @@ def convert_to_instruction_format(path_to_dataset, path_to_json):
     json.dump(programs_list, open(path_to_json, 'w'), indent=4, sort_keys=True)
 
 
+def print_io_pairs():
+    dataset = load_dataset(
+        "/data/xiachunwei/Datasets/exebench",
+        split='train_real_simple_io')  # , use_auth_token=True)
+    for row in dataset.select(range(0, 10)):
+        print(row['path'])
+        for i, o in zip(row['real_io_pairs']['input'],
+                        row['real_io_pairs']['output']):
+            print("input: ", i, "output: ", o)
+        print("*" * 20)
+
+
+def test_eval_assembly():
+    dataset = load_from_disk(
+        "/data/xiachunwei/Datasets/exebench/train_real_simple_io-llvm-assembly-batch-clang-15"
+    )
+    for row in dataset.select(range(0, 10)):
+        print(row['path'])
+        print(eval_assembly(row, row["asm"]['code'][-1]))
+
+
 if __name__ == '__main__':
     # dump_sample_for_splits("/data/xiachunwei/Datasets/exebench")
-    test_compile_and_run_exebench_sample("/data/xiachunwei/Datasets/exebench",
-                             num_samples=100)
-    # convert_to_instruction_format("/data/xiachunwei/Datasets/exebench/train_real_simple_io-llvm-assembly-batch",
-    #                      "/data/xiachunwei/Datasets/exebench/train_real_simple_io-llvm-assembly-batch.json")
+    # test_compile_and_run_exebench_sample("/data/xiachunwei/Datasets/exebench",
+    #                          num_samples=100)
+    # path_to_exebench_dataset = "/data/xiachunwei/Datasets/exebench"
+    # path_to_saved_dataset = "/data/xiachunwei/Datasets/exebench/train_real_simple_io-llvm-assembly-batch-clang-15"
+    # get_all_assembly(path_to_exebench_dataset, path_to_saved_dataset)
+    # convert_to_instruction_format(path_to_saved_dataset,
+    #                      f"{path_to_saved_dataset}.json")
+    # test_default_compile_and_run_exebench_sample(path_to_exebench_dataset,
+    #                          num_samples=100)
+    # print_io_pairs()
+    test_eval_assembly()
--- exebench/__init__.py
+++ exebench/__init__.py
@@ -2,7 +2,7 @@ import math
 import json
 from pathlib import Path
 import subprocess
-from typing import Optional, Tuple
+from typing import Optional, Tuple, Dict
 import tempfile
 import contextlib
 import os
@@ -15,11 +15,14 @@ import re
 
 # Set up logging
 logging.basicConfig(
-    format='%(asctime)s - %(levelname)s - %(filename)s - Line: %(lineno)d - %(message)s',
-    level=logging.DEBUG
-)
+    format=
+    '%(asctime)s - %(levelname)s - %(filename)s - Line: %(lineno)d - %(message)s',
+    level=logging.DEBUG)
 
-__all__ = ['diff_io', 'Wrapper', 'exebench_dict_to_dict', 'LLVMAssembler', 'cpp2ass', 'll2ass']
+__all__ = [
+    'diff_io', 'Wrapper', 'exebench_dict_to_dict', 'LLVMAssembler', 'cpp2ass',
+    'll2ass'
+]
 
 __version__ = 0.1
 
@@ -30,10 +33,19 @@ _ROOT_PATH_FOR_JSON_HPP = os.path.dirname(__file__)
 _SYNTH_LIBS_PATH = os.path.dirname(__file__)
 
 
-def _run_command(command: str, stdin: Optional[str] = None, timeout: Optional[int] = _DEFAULT_CMD_TIMEOUT) -> Tuple[str, str]:
-    output = subprocess.run(command.split(), capture_output=True, text=True, input=stdin, timeout=timeout)
-    stdout = output.stdout.decode('utf-8') if isinstance(output.stdout, bytes) else output.stdout
-    stderr = output.stderr.decode('utf-8') if isinstance(output.stderr, bytes) else output.stderr
+def _run_command(
+        command: str,
+        stdin: Optional[str] = None,
+        timeout: Optional[int] = _DEFAULT_CMD_TIMEOUT) -> Tuple[str, str]:
+    output = subprocess.run(command.split(),
+                            capture_output=True,
+                            text=True,
+                            input=stdin,
+                            timeout=timeout)
+    stdout = output.stdout.decode('utf-8') if isinstance(
+        output.stdout, bytes) else output.stdout
+    stderr = output.stderr.decode('utf-8') if isinstance(
+        output.stderr, bytes) else output.stderr
     return output.returncode, stdout, stderr
 
 
@@ -51,17 +63,25 @@ def _cleanup(path_pattern):
 
 
 @contextlib.contextmanager
-def _get_tmp_path(content: Optional[str] = None, suffix: Optional[str] = None, delete=False) -> str:
+def _get_tmp_path(content: Optional[str] = None,
+                  suffix: Optional[str] = None,
+                  delete=False) -> str:
     prefix = _get_host_process_id()
     try:
-        with tempfile.NamedTemporaryFile(prefix=prefix, suffix=suffix, delete=delete, mode='w+') as ntf:
+        with tempfile.NamedTemporaryFile(prefix=prefix,
+                                         suffix=suffix,
+                                         delete=delete,
+                                         mode='w+') as ntf:
             if content:
                 ntf.write(content)
                 ntf.flush()
             yield ntf.name
     except OSError:
         _cleanup(os.path.join(tempfile.gettempdir(), prefix, '*'))
-        with tempfile.NamedTemporaryFile(prefix=prefix, suffix=suffix, delete=delete, mode='w+') as ntf:
+        with tempfile.NamedTemporaryFile(prefix=prefix,
+                                         suffix=suffix,
+                                         delete=delete,
+                                         mode='w+') as ntf:
             if content:
                 ntf.write(content)
                 ntf.flush()
@@ -69,47 +89,58 @@ def _get_tmp_path(content: Optional[str] = None, suffix: Optional[str] = None, d
 
 
 class _Assembler:
-    def __call__(self, c_deps, func_c_signature, func_assembly, cpp_wrapper) -> Path:
+    def __call__(self, c_deps, func_c_signature, func_assembly,
+                 cpp_wrapper) -> Path:
         raise NotImplemented
 
 
 class _DefaultAssembler(_Assembler):
-    def __call__(self, c_deps, func_c_signature, func_assembly, cpp_wrapper) -> Path:
-        with _get_tmp_path(content=None, suffix='.x', delete=False) as executable_path:
+    def __call__(self, c_deps, func_c_signature, func_assembly,
+                 cpp_wrapper) -> Path:
+        with _get_tmp_path(content=None, suffix='.x',
+                           delete=False) as executable_path:
             c_deps += f'\nextern {func_c_signature};\n'
             with _get_tmp_path(content=c_deps, suffix='.c') as c_deps_path:
-                cpp_wrapper = re.sub(
-                    r'extern\s\"C\"\s\{\s.*\s\}', 'extern "C" \n{\n#include "' + c_deps_path + '"\n}\n',
-                    cpp_wrapper) # replace tmp path
+                cpp_wrapper = re.sub(r'extern\s\"C\"\s\{\s.*\s\}',
+                                     'extern "C" \n{\n#include "' +
+                                     c_deps_path + '"\n}\n',
+                                     cpp_wrapper)  # replace tmp path
                 with _get_tmp_path(content=cpp_wrapper, suffix='.cpp') as cpp_path, \
                         _get_tmp_path(content=func_assembly, suffix='.s') as s_path:
 
                     cmd = f'g++ -fpermissive -O0 -o {executable_path} {cpp_path} {s_path} -I {_ROOT_PATH_FOR_JSON_HPP} -I{_SYNTH_LIBS_PATH}'
 
-                    _, stdout, stderr = _run_command(cmd)
-                    
+                    returncode, stdout, stderr = _run_command(cmd)
+                    if returncode != 0:
+                        logging.error(f"Executing {cmd} failed with {stderr}")
+                        return None
 
         return Path(executable_path)
 
 
-def _compile_exe_path(c_deps, func_c_signature, func_assembly, cpp_wrapper, assembler_backend):
-    return assembler_backend(c_deps, func_c_signature, func_assembly, cpp_wrapper)
+def _compile_exe_path(c_deps, func_c_signature, func_assembly, cpp_wrapper,
+                      assembler_backend):
+    return assembler_backend(c_deps, func_c_signature, func_assembly,
+                             cpp_wrapper)
+
 
 # API
+
 
 def contrain_error(content: str) -> bool:
     pattern = re.compile(r'error|Error:|ERROR|Error')
     return bool(pattern.search(content))
 
-def cpp2ass(cpp_code: str, opt_level = "-O2") -> Tuple[bool, str, str]:
+
+def cpp2ass(cpp_code: str, opt_level="-O2") -> Tuple[bool, str, str]:
     """Converts C++ code to assembly code and llvm using clang and llc.
     
     """
-    success = True
+    success, ll_code, s_code = True, None, None
     try:
         with _get_tmp_path(content=cpp_code, suffix='.c') as cpp_path:
             with _get_tmp_path(content=None, suffix='.s') as s_path:
-                cmd = f"clang {opt_level} -emit-llvm -o {str(cpp_path)}.ll -c {cpp_path}"
+                cmd = f"clang {opt_level} -emit-llvm -S -o {str(cpp_path)}.ll -c {cpp_path}"
                 returncode, stdout, stderr = _run_command(cmd)
                 if returncode != 0 or contrain_error(stderr):
                     success = False
@@ -121,10 +152,8 @@ def cpp2ass(cpp_code: str, opt_level = "-O2") -> Tuple[bool, str, str]:
                     logging.error(f"Executing {cmd} got {stderr}")
                 with open(s_path, 'r') as f:
                     s_code = f.read()
-                ll_code = None
-                # TODO(Extract LLVM IR from here)
-                # with open(str(cpp_path), 'r') as f:
-                #     ll_code = f.read()
+                with open(f"{str(cpp_path)}.ll", 'r') as f:
+                    ll_code = f.read()
                 return success, ll_code, s_code
     except Exception as e:
         success, ll_code, s_code = False, None, None
@@ -146,33 +175,50 @@ def ll2ass(ll_code: str) -> str:
 
 
 class LLVMAssembler(_Assembler):
-    def __call__(self, c_deps, func_c_signature, func_assembly, cpp_wrapper) -> Path:
-        with _get_tmp_path(content=None, suffix='.x', delete=False) as executable_path:
+    def __call__(self, c_deps, func_c_signature, func_assembly,
+                 cpp_wrapper) -> Path:
+        with _get_tmp_path(content=None, suffix='.x',
+                           delete=False) as executable_path:
             c_deps += f'\nextern {func_c_signature};\n'
             with _get_tmp_path(content=c_deps, suffix='.c') as c_deps_path:
-                cpp_wrapper = re.sub(
-                    r'extern\s\"C\"\s\{\s.*\s\}', 'extern "C" \n{\n#include "' + c_deps_path + '"\n}\n',
-                    cpp_wrapper) # replace tmp path
+                cpp_wrapper = re.sub(r'extern\s\"C\"\s\{\s.*\s\}',
+                                     'extern "C" \n{\n#include "' +
+                                     c_deps_path + '"\n}\n',
+                                     cpp_wrapper)  # replace tmp path
                 with _get_tmp_path(content=cpp_wrapper, suffix='.cpp') as cpp_path, \
                         _get_tmp_path(content=func_assembly, suffix='.s') as s_path:
 
                     cmd = f'clang++ -fpermissive -O0 -o {executable_path} {cpp_path} {s_path} -I {_ROOT_PATH_FOR_JSON_HPP} -I{_SYNTH_LIBS_PATH}'
-                    
+
                     returncode, stdout, stderr = _run_command(cmd)
                     if returncode != 0 or contrain_error(stderr):
                         logging.error(f"Executing {cmd} failed with {stderr}")
                         return None
-        
+
         return Path(executable_path)
 
 
 class Wrapper:
-    def __init__(self, c_deps, func_c_signature, func_assembly, cpp_wrapper, 
-                assembler_backend=_DefaultAssembler(), func_def: str=None, ll_code: str=None):
-        self._compiled_exe_path = self._compile_exe_path(c_deps, func_c_signature, func_assembly, cpp_wrapper, assembler_backend, func_def, ll_code)
+    def __init__(self,
+                 c_deps,
+                 func_c_signature,
+                 func_assembly,
+                 cpp_wrapper,
+                 assembler_backend=_DefaultAssembler(),
+                 func_def: str = None,
+                 ll_code: str = None):
+        self._compiled_exe_path = self._compile_exe_path(
+            c_deps, func_c_signature, func_assembly, cpp_wrapper,
+            assembler_backend, func_def, ll_code)
 
     @staticmethod
-    def _compile_exe_path(c_deps, func_c_signature, func_assembly, cpp_wrapper, assembler_backend, func_def: str = None, ll_code: str = None):
+    def _compile_exe_path(c_deps,
+                          func_c_signature,
+                          func_assembly,
+                          cpp_wrapper,
+                          assembler_backend,
+                          func_def: str = None,
+                          ll_code: str = None):
         success = True
         if func_def is not None:
             success, _, func_assembly = cpp2ass(func_def)
@@ -180,19 +226,23 @@ class Wrapper:
             func_assembly = ll2ass(ll_code)
         if (func_def is not None or ll_code is not None) and not success:
             return None
-        return _compile_exe_path(c_deps, func_c_signature, func_assembly, cpp_wrapper, assembler_backend)
+        return _compile_exe_path(c_deps, func_c_signature, func_assembly,
+                                 cpp_wrapper, assembler_backend)
 
     def __call__(self, inp, return_stdout_and_stderr=False):
         executable = self._compiled_exe_path
         if executable is None:
             return None
-        with _get_tmp_path(content=None, suffix='.json') as input_tmp_json_path:
-            output_file = ''.join(input_tmp_json_path.split(".")[:1]) + '-out.json'
+        with _get_tmp_path(content=None,
+                           suffix='.json') as input_tmp_json_path:
+            output_file = ''.join(
+                input_tmp_json_path.split(".")[:1]) + '-out.json'
 
             with open(input_tmp_json_path, 'w') as f:
                 json.dump(inp, f)
 
-            returncode, stdout, stderr = _run_command(f'{executable} {input_tmp_json_path} {output_file}')
+            returncode, stdout, stderr = _run_command(
+                f'{executable} {input_tmp_json_path} {output_file}')
 
             with open(output_file, 'r') as f:
                 output = json.load(f)
@@ -249,3 +299,55 @@ def exebench_dict_to_dict(exebench_dict):
     keys = exebench_dict['var']
     values = exebench_dict['value']
     return _fix_nested_dict({k: v for k, v in zip(keys, values)})
+
+
+def preprocessing_c_deps(row) -> str:
+    c_deps = ""
+    c_deps += row['real_deps'] + "\n" if row['real_deps'] is not None else ""
+    replacements = ["typedef int bool;"]
+    for r in replacements:
+        if c_deps.find(r) != -1:
+            c_deps = c_deps.replace(r, "").strip()
+
+    if c_deps == "" and row['synth_deps'] is not None:
+        c_deps += row['synth_deps'] + "\n"
+    for r in replacements:
+        if c_deps.find(r) != -1:
+            c_deps = c_deps.replace(r, "").strip()
+    return c_deps + "\n"
+
+
+def eval_assembly(row: Dict, assembly: str) -> bool:
+    success = True
+    synth_wrapper = None
+    try:
+        c_deps = preprocessing_c_deps(row)
+        synth_wrapper = Wrapper(
+            c_deps=c_deps + '\n',
+            func_c_signature=row['func_head_types'].replace('extern', ''),
+            func_assembly=assembly,
+            cpp_wrapper=row['real_exe_wrapper'],
+            assembler_backend=LLVMAssembler())
+        count, total = 0, len(row['real_io_pairs']['input'])
+        for i, o in zip(row['real_io_pairs']['input'],
+                        row['real_io_pairs']['output']):
+            observed_output = synth_wrapper(
+                exebench_dict_to_dict(i))  # Run synthetic
+            if observed_output is None:
+                logging.error('Error: The code could not be compiled')
+                success = False
+                return success
+            count += 1 if diff_io(
+                observed_output=observed_output,
+                expected_output=exebench_dict_to_dict(o)) else 0
+        success = (count == total)
+        if not success:
+            logging.info(
+                f"Error for {row['path']} total cases {total}, success cases {count}"
+            )
+    except Exception as e:
+        logging.error(f"Error for {row['path']}")
+        logging.error(e)
+        success = False
+    finally:
+        return success
